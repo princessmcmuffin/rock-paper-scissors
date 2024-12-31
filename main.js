fef722c190b576525f966b154c0bab1edbb25290
Steph <stephanie@mpb.com>
2024-12-31 15:44:59 +0000

    Update main.js
    Updated my code 7 months later, much more efficient!

--- main.js
+++ main.js
@@ -1,45 +1,41 @@
-const getUserChoice = userInput  => {
-    userInput = userInput.toLowerCase();
-    if (userInput === 'rock' || userInput === 'paper' || userInput === 'scissors') {
-      return userInput
-    } else {
-      console.log('Error')
-    }
+const choices = ['rock', 'paper', 'scissors'];
+
+const getUserChoice = (UserInput) => {
+  UserInput = UserInput.toLowerCase();
+
+  UserInput = (UserInput === choices[0] || UserInput === choices[1] || UserInput === choices[2])  ? UserInput : console.log('Input error')
+
+  return UserInput
+}
+
+const getComputerChoice = () => {
+  const randomIndex = Math.floor(Math.random() * 3);
+  return choices[randomIndex];
+}
+
+const determineWinner = (getUserChoice, getComputerChoice) => {
+  if (userChoice === computerChoice) {
+    return 'It\'s a tie!';
   }
-  
-  function getComputerChoice() {
-    const num = Math.floor(Math.random() * 3)
-    if (num === 0) {
-      return 'rock'
-    } else if (num === 1) {
-      return 'paper'
-    } else {
-      return 'scissors'
-    }
-  }
-  
-  function determineWinner(userChoice, computerChoice) {
-    if (userChoice === computerChoice) {
-      return "User: " + userChoice + " | Comp: " + computerChoice + " | It was a tie"
-    } if (userChoice === 'rock') {
-        if (computerChoice === 'paper') {
-          return "User: " + userChoice + " | Comp: " + computerChoice + " | Computer won"
-      } else {
-        return "User: " + userChoice + " | Comp: " + computerChoice + " | Human won"
-      }
-    } else if (userChoice === 'paper') {
-        if (computerChoice === 'scissors') {
-          return "User: " + userChoice + " | Comp: " + computerChoice + ' | Computer won'
-        } else {
-          return "User: " + userChoice + " | Comp: " + computerChoice + ' | Human won'
-        }
-    } else if (userChoice === 'scissors') {
-      if (computerChoice === 'rock') {
-        return "User: " + userChoice + " | Comp: " + computerChoice + " | Computer won"
-      } else {
-        return "User: " + userChoice + " | Comp: " + computerChoice + " | Human won"
-      }
-    }
-  }
-  
-  console.log(determineWinner(getUserChoice("rock"), getComputerChoice()))
+
+  const winConditions = {
+    rock: 'scissors',
+    paper: 'rock',
+    scissors: 'paper'
+  };
+
+  return winConditions[userChoice] === computerChoice 
+    ? 'You win!' 
+    : 'Computer wins!';
+};
+
+const playGame = () => {
+  userChoice = getUserChoice('rock')
+  console.log("You have chosen " + userChoice)
+  computerChoice = getComputerChoice()
+  console.log("The computer has chosen " + computerChoice)
+
+  console.log(determineWinner(userChoice, computerChoice))
+}
+
+playGame()
